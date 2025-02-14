6d171ee02102ae2a6c49144dda413d400564367e
STEFANOVITCH-Ilann-24019037 <ilann.stefanovitch@etu.univ-amu.fr>
2025-02-14 11:14:38 +0100

    Fin du tp

--- src/tp3exosYaip7/Visit.java
+++ src/tp3exosYaip7/Visit.java
@@ -40,5 +40,17 @@ public class Visit {
 		return "Customer :"+customer.toString()+" Expense: "+ getTotalExpense() +"$";
 	}
 	
+	public static void main(String[] args) {
+		Customer c = new Customer("null");
+		System.out.println(c.getName());
+		c.setMember(true);
+		c.setMemberType("TropBeau");
+		System.out.println(c.toString());
+		
+		Date date =new Date();
+		Visit v = new Visit("Coucou",date);
+		v.setProductExpense(90);
+		System.out.println(v.getProductExpense());
+	}
 
 }
